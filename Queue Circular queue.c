48ce320b1515b5dfb7c2790cdd95d4ea482b5a3f
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-08-23 22:46:37 +0700

    Circular Queue Data Structure Array Implementation
    Circular Queue Data Structure Array Implementation 
    Circular Queue ( DS) Array Implementation

--- Queue Circular queue.c
+++ Queue Circular queue.c
@@ -28,7 +28,8 @@ void dequeue();
 //function to Check if the queue is empty or not
 int isEmpty();
 
-int fron ();
+//function to  find the front element in queue
+int find_front();
 
 //function to traverse queue and Print the queue element
  void trevers();
@@ -48,9 +49,10 @@ int main(int argc, char* argv[])
         int element; // element
         printf("Circular Queue data structure implementation : \n");
         printf("1 : enqueue                                  :\n");
-        printf("2 : dequeue                                  :\n"); //delete
-        printf("3 : print all the element of the queue       :\n");
-        printf("4 : clear queue                              :\n"); //delete all the element in queue
+        printf("2 : dequeue                                  :\n"); //delete front
+        printf("3 : find front                               : \n");
+        printf("4 : print all the element of the queue       :\n");
+        printf("5 : clear queue                              :\n"); //delete all the element in queue
         printf("0 : quit                                     :\n");
 
         // asking user to enter choice
@@ -59,25 +61,33 @@ int main(int argc, char* argv[])
 
          switch(ch)
          {
-            // case 1 enqueue new element
+            // case 1 insert new element to Circular Queue
             case 1:
                 printf("Enter element to enqueue : ");
                 scanf("%d",&element);
                 enqueue(element); // call enqueue function
             break;
 
-            // case 2 dequeue element from queue
+            // case 2 dequeue element from Circular Queue
             case 2:
                 dequeue(); // call dequeue function
             break;
 
-            // case 3 traverse queue
+            // case 3 find the front element in Circular Queue
             case 3:
+              element = find_front(); // call find_front()
+              if(element != -990)
+                 printf("the front element in Circular Queue is -->  %d\n",element);
+
+            break;
+
+            // case 4 traverse Circular Queue and print it value
+            case 4:
                 trevers(); // call printQueue function
             break;
 
-            // case 4 clear all the element in queue
-            case 4:
+            // case 5 clear all the element in queue
+            case 5:
                 clearQueue(); // call clearQueue() function
             break;
 
@@ -98,23 +108,28 @@ int main(int argc, char* argv[])
 }/** End of main function */
 
 
-/** A utility function to push new given element to queue */
+/**
+  A utility function to push new given element to queue
+  Reference in future :---->
+ https://youtu.be/dn01XST9-bI  */
 
 void enqueue(int element)
 {
     if(isFull()) // queue is full condition
     {
         printf("Queue is Full!!\n");
+        return;
     }
-    else if(isEmpty()) // queue is isempty condition
+
+    else if(isEmpty()) // queue is is empty condition
     {
         first = last = 0;
         queue[last] = element;  // first element in queue
     }
-    else
+    else // queue is not full or empty we can add at last
     {
-        last =  (last + 1 ) %  CAPCITY ;  // make sure to make crricle queue
-        queue[last] = element; // add at last as ususal
+        last  = (last + 1 ) %  CAPCITY ;  // make sure to Circular queue
+        queue[last] = element; // add at last as usual
     }
     printf("%d  been push to queue\n", element); // inform  user the element is been added
 
@@ -123,37 +138,43 @@ void enqueue(int element)
 
 /**
      utility function to dequeue(remove)first
-     element in queue (first in first out)  */
+     element in queue (first in first out)
+     Reference in future :---->
+     https://youtu.be/dn01XST9-bI*/
 
 void dequeue()
 {
-    // queue is empty condition
-    //if queue is empty then we dont have any element to dequeue
-    if(!isEmpty())
-       printf ("Queue is empty \n");
+    int deleted;  // variable declaration to keep the element before deleted
 
-    else if (first == last)  // if only one element on list
+    //if queue is empty then we dont have any element to dequeue
+    if(isEmpty()) // queue is empty condition
     {
-        //printf (" %d been dequeue",queue[first]);
+       printf ("Queue is empty \n");
+       return;
+    }
+
+    else if (first == last)  // case when queue have only one element on list
+    {
+        deleted = queue[first]; // keep the elmenet
 
         first = last = -1;   // reset first and last back to -1
 
         // mean if queue have only one element after
         // removing that one queue will be empty
     }
-    else
+    else// queue is not empty and have more than one element we can remove from start
     {
-        // printf (" %d been dequeue",queue[first]);
-
+        deleted = queue[first]; // keep the elmenet
         first  = (first + 1 ) %  CAPCITY ;  // make sure to make Circular queue
     }
+
+     printf ("%d been dequeued  \n", deleted);  // inform user the job is done
 
 }/** End of dequeue() */
 
 
 /**
-   Utility function to Check if the queue is empty or not here
-   Am saying that queue is empty if and only if (first == last) */
+   Utility function to Check if the queue is empty or not here */
 
 int isEmpty()
 {
@@ -168,7 +189,10 @@ int isEmpty()
 /**
    Utility function to Check if the queue is full or not here
    Am saying that queue is full if and only if
-   ((last + 1 ) % CAPCITY == first) */
+   ((last + 1 ) % CAPCITY == first)
+
+   Reference in future :---->
+ https://youtu.be/dn01XST9-bI*/
 
 int isFull()
 {
@@ -181,17 +205,40 @@ int isFull()
 } /** End of isFull() */
 
 
-/** Utility function to traverse the queue and print all the element */
+/** Utility function to traverse the queue and print
+  all the element
+  Reference in future :---->
+ https://youtu.be/dn01XST9-bI*/
 
 void trevers()
 {
     /** while queue is not empty dequeue
        print the front element and dequeued */
-    while(!isEmpty())
+    //while(!isEmpty())
+    //{
+        //printf("\n%d \n",find_front()); // print the element first
+        //dequeue();               // now  dequeue that one out
+    //}
+
+    /** also  we can write in different way as blow */
+
+    if (isEmpty()) // if queue is already empty
     {
-        printf("\n%d \n",fron()); // print the element first
-        dequeue();               // now  dequeue that one out
+        printf ("Queue is empty \n");
+        return;
     }
+    int  i = first; // declare counter i at let start from first
+    while(i != last) // while condition is true go in loop and print
+    {
+        printf("%d\t", queue[i]);
+
+        i = i + 1  % CAPCITY; // Circularly increment i
+    }
+
+    /* after while loop now i == last but we have one
+       element left at last index let printed */
+
+    printf("%d\t\n", queue[last]);  // also can be queue[last]
 
 } /**END of trevers()*/
 
@@ -202,9 +249,14 @@ void trevers()
 void clearQueue()
 {
     if (isEmpty()) // if queue is already empty
-        printf ("Queue is empty so no element to clear\n");
+    {
+        printf ("Queue is empty \n");
+        return;
+    }
     else
-     first = last; // deleting all the element
+     first = last = -1; // deleting all the element
+
+     printf("Circular Queue is been cleared \n"); // inform user the job is done
 
 } /** End of clearQueue() */
 
@@ -214,17 +266,14 @@ void clearQueue()
   Utility function return front of the queue
   first element enter in the queue  */
 
-
-int fron()
+int find_front()
 {
     if(isEmpty()) // queue is empty condition
     {
         printf ("Queue is empty \n");
-        return  1;
+        return  -990;
     }
     return queue[first]; // return first element in queue
 
 } /** End of fron()() */
-
-
 
