2d9885e086fae1a112a686ed22999d0c2a3c8e8e
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-10-31 06:23:31 +0700

    Prim's Minimum Spanning Tree (TO DO)

---
+++ graph Minimum Spanning Tree.c
@@ -0,0 +1,31 @@
+/**
+* [PROG]		: Implementation Minimum Spanning Tree
+                   using | Prim's Algorithm
+
+* [AUTHOR]		: Saddam Arbaa <saddamarbaas@gmail.com>
+
+    C program for Prim's Algorithm to find Minimum Spanning Tree
+     of a given connected, undirected and weighted graph
+
+
+   reference
+  (1) https://youtu.be/k9jemw3SZe0
+  (2) https://youtu.be/7FtGk9yr66A
+
+  */
+
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+// the  Driver Code
+int main()
+{
+
+    printf("code is not complete yet\n");
+
+
+
+    return 0; // signal to operating system program ran fine
+}
