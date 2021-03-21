b7a6e72b6a57f14826bbb56783156f3a88cf4b02
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2021-03-21 14:29:59 +0700

    (Minimum Waiting Time) Greedy Algorithm

--- 05 AlgoExpert.js
+++ 05 AlgoExpert.js
@@ -2,12 +2,16 @@
  Topic: Greedy Algorithm; Difficulty: Easy
 
  Prompt: 
- You're given a non-empty array of positive integers representing the amounts of time that specifies 'queries' take to execute. Only one  'query' can be executed at a time, but the queries can be executed in any order.
+ You're given a non-empty array of positive integers representing the amounts of time that specifies 'queries' take to execute. 
+ Only one  'query' can be executed at a time, but the queries can be executed in any order.
 
- A query's 'waiting time' is defined as: the amount of time that it must wait before its execution starts. In other words, if a query is executed second, then its waiting time is the duration of the first query; if a query is executed third, then its waiting time is the sum of the durations of the first two queries.
+ A query's 'waiting time' is defined as: the amount of time that it must wait before its execution starts. In other words, 
+ if a query is executed second, then its waiting time is the duration of the first query; if a query is executed third, 
+ then its waiting time is the sum of the durations of the first two queries.
 
  Write a function that returns the minimum amount of total waiting time for all of the queries.
- For example, if you are given the queries of durations [ 1, 4, 5], then the total waiting time if the queries were executed in the order of [5, 1, 4] would be: (0) + (5) + (5+1) =11.
+ For example, if you are given the queries of durations [ 1, 4, 5], then the total waiting time if the queries were 
+ executed in the order of [5, 1, 4] would be: (0) + (5) + (5+1) =11.
 
  The first query of duration: 5 would be executed immediately, so its waiting time would be 0.
  
