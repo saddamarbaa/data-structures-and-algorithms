91030f0e5ab6f88522cd5659347496ead4fd6658
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2021-04-21 18:52:47 +0700

    Doubly LinkedList Operations Implementation in Javascript

--- 10 doubly LinkedList.js
+++ 10 doubly LinkedList.js
@@ -1,7 +1,27 @@
 /** @format */
 
+/*** 
+    [PROGRAM] : Doubly linkedlist Data Structure Complete Implementation
+    [AUTHOR]  :  Saddam Arbaa
+    [Email]   :  <saddamarbaas@gmail.com>
+
+    Javascript Program for Complete implementation of Doubly linked list data structure.
+
+    A linked list is a linear data structure, in which the elements
+    are not stored at contiguous memory locations.
+    The elements in a linked list are linked using pointers
+    (entity that point to the next element)
+
+    Doubly Linked list is a type of Linked List Data structure
+    in which a node contain of three parts(filed)
+    (1) node data,
+    (2) pointer to the next node (next pointer)
+    (3) pointer to the previous node(previous pointer).
+ */
+
 // stackblitz Testing Link
 // https://stackblitz.com/edit/js-zlittw
+
 
 // Node Class
 class Node {
@@ -31,7 +51,7 @@ class DoublyLinkedList {
 			// if already some element are in the linked list we
 			// have to add the new node at the Beginning of the list
 
-			/** link changes */
+			// link changes
 			newNode.next = this.head; // right side connection first
 			this.head.prev = newNode; // left side connection second
 			this.head = newNode; // let Head point to the new node
@@ -113,6 +133,40 @@ class DoublyLinkedList {
 
 		// Time complexity of length() is O(n)
 	}
+
+	// A utility function to Reverse a linked list (Iterative method)
+	reverse() {
+		// local variables declaration
+		let current, previous, nextNode;
+
+		previous = null; // for Previous node (only to keep address of last node)
+		current = this.head; // for current node
+		nextNode = null; // for next node
+
+		if (this.head === null) {
+			// linked is empty Case
+			console.log("Doubly linked list is Empty!!!");
+			return; // we are done
+		}
+
+		while (current !== null) {
+			// link changes
+			nextNode = current.next; // move nextNode point to next node
+			current.next = current.prev; // swap between current -> next and current -> prev
+			current.prev = nextNode; // swap between current -> next and current -> prev
+			previous = current; // I use Previous only to get address of last node and assign to head pointer
+			current = nextNode; // move current to next until you reach null which is end of list
+
+			// after that go back and loop again
+		}
+		// after while loop now let head) point to last node
+
+		this.head = previous; // Reverse (head pointer) to point to last node
+
+		console.log("Doubly linked list is been Reversed");
+
+		// Time complexity of Reverse() is O(n)
+	}
 }
 
 const object1 = new DoublyLinkedList();
@@ -120,5 +174,9 @@ for (let i = 1; i <= 10; i++) {
 	// insert nodes from 1 to 10
 	object1.append(i);
 }
+
+object1.traverse();
+
+object1.reverse();
 
 object1.traverse();
