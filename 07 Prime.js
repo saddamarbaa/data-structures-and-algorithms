20c598d0064b8ffe79b51982437f176810003566
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2021-05-09 14:51:21 +0700

    Verify a Prime number Trial division method in O sqrt(N)

--- 07 Prime.js
+++ 07 Prime.js
@@ -1,5 +1,18 @@
+/** @format */
+
 function isPrime(num) {
 	for (let i = 2; i <= num / 2; i++) {
+		if (num % i === 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+console.log(isPrime(5));
+
+function isPrime(num) {
+	for (let i = 2; i <= Math.sqrt(num); i++) {
 		if (num % i === 0) {
 			return false;
 		}
