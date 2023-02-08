6ebbe1d65f54c4b9746c2e501206b5557a854b04
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2023-02-08 07:47:37 +0700

    9. Palindrome Number

---
+++ src/LeetCode/easy/02 Palindrome Number.java
@@ -0,0 +1,40 @@
+/*
+
+  Given an integer x, return true if x is a palindrome, and false otherwise
+  
+  link
+  https://leetcode.com/problems/palindrome-number/
+  
+   Example 1:
+   Input: x = 121
+   Output: true
+   Explanation: 121 reads as 121 from left to right and from right to left.
+   
+   
+   Example 2:
+   Input: x = -121
+   Output: false
+   Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome
+*/
+
+public class PalindromeCheck {
+  public static boolean isPalindrome(int number) {
+    int originalNumber = number;
+    int reverse = 0;
+    while (number != 0) {
+      int lastDigit = number % 10;
+      reverse = reverse * 10 + lastDigit;
+      number /= 10;
+    }
+    return originalNumber == reverse;
+  }
+
+  public static void main(String[] args) {
+    int number = 121;
+    if (isPalindrome(number)) {
+      System.out.println(number + " is a palindrome");
+    } else {
+      System.out.println(number + " is not a palindrome");
+    }
+  }
+}
