a507beb19f1d476638b832b3853be2c924325145
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2023-03-13 07:30:23 +0700

    167. Two Sum II - Input Array Is Sorted

--- src/LeetCode/medium/03 Two Sum II - Input Array Is Sorted.java
+++ src/LeetCode/medium/03 Two Sum II - Input Array Is Sorted.java
@@ -1,7 +1,6 @@
 /*
 67. Two Sum II - Input Array Is Sorted
 Medium
-Companies
 Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
 
 Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
