1b9b4a8a4fa1460121c4c2bbc3b21f2874828521
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2023-03-24 16:23:53 +0700

    1480. Running Sum of 1d Array

--- src/LeetCode/easy/ 1480. Running Sum of 1d Array.java
+++ src/LeetCode/easy/ 1480. Running Sum of 1d Array.java
@@ -1,8 +1,6 @@
 /*
 1480. Running Sum of 1d Array
 Easy
-
-Companies
 Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
 
 Return the running sum of nums.
