30a123d22e3b206a98dc52dc52b63c2f75968988
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2023-03-24 16:19:45 +0700

    1886. Determine Whether Matrix Can Be Obtained By Rotation

--- src/LeetCode/easy/1886. Determine Whether Matrix Can Be Obtained By Rotation.java
+++ src/LeetCode/easy/1886. Determine Whether Matrix Can Be Obtained By Rotation.java
@@ -1,13 +1,9 @@
 /*
 1886. Determine Whether Matrix Can Be Obtained By Rotation
 Easy
-Companies
 Given two n x n binary matrices mat and target, return true if it is possible to make mat equal to target by rotating mat in 90-degree increments, or false otherwise.
 
- 
-
 Example 1:
-
 
 Input: mat = [[0,1],[1,0]], target = [[1,0],[0,1]]
 Output: true
