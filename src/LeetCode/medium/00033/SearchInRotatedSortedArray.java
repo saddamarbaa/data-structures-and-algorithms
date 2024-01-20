cc1386ce6499626d812895d16edcac0e309c0fe7
saddamarbaa <saddamarbaas@gamil.com>
2024-01-20 11:10:21 +0700

    Print all subsets of an array using recursion

---
+++ src/LeetCode/medium/00033/SearchInRotatedSortedArray.java
@@ -0,0 +1,6 @@
+
+public class SearchInRotatedSortedArray {
+  public static void main(String[] args) {
+    System.out.println(2);
+  }
+}
--- src/Recursion/printSubsets.java
+++ src/Recursion/printSubsets.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Subsets {
+public class PrintSubsets {
     public static void main(String[] args) {
         int[] arr1 = {1, 2, 3};
         System.out.println("Subsets of " + Arrays.toString(arr1) + ":");
