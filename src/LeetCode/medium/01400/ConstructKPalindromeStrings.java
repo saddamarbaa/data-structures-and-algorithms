5174e6286907d64c371a4bfe48f57eb401b2d381
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2025-01-25 22:44:00 -0800

    1400. Construct K Palindrome Strings

--- src/LeetCode/medium/01400/ConstructKPalindromeStrings.java
+++ src/LeetCode/medium/01400/ConstructKPalindromeStrings.java
@@ -24,6 +24,34 @@ Output: true
 Explanation: The only possible solution is to put each character in a separate string.
  */
 public class ConstructKPalindromeStrings {
+
+      public boolean canConstruct2(String s, int k) {
+        // If k is greater than the length of the string, it's impossible
+        if (k > s.length()) {
+            return false;
+        }
+
+        // Use a HashMap to count the frequency of each character
+        HashMap<Character, Integer> charCountMap = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            charCountMap.put(c, charCountMap.getOrDefault(c, 0) + 1);
+        }
+
+        // Count how many characters have an odd frequency
+        int oddCount = 0;
+        for (int count : charCountMap.values()) {
+            if (count % 2 != 0) {
+                oddCount++;
+            }
+        }
+
+        // To divide into k palindrome strings:
+        // - At most k characters can have an odd frequency, because each palindrome string can only
+        //   have at most one character with an odd frequency in the center.
+        // - The number of odd frequency characters must be less than or equal to k.
+        return oddCount <= k;
+    }
+
     
     public boolean canConstruct(String s, int k) {
         // If k is greater than the length of the string, it's impossible
